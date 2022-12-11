71696322b3f127e53fb791323f7f6caee10211c3
mindsers <nathanael@cherrier.co>
2022-12-11 16:25:10 +0100

    Add language switch on footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,21 @@
 import { useTranslations } from 'next-intl'
+import Link from 'next/link'
 import styles from './Footer.module.css'
 
 export function Footer() {
   const t = useTranslations('Footer')
-  return <footer className={styles.footer}>&copy;{t('copyright')}</footer>
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <Link href={''} locale="fr">
+          🇫🇷 French
+        </Link>
+         - 
+        <Link href={''} locale="en">
+          🇺🇸 English
+        </Link>
+      </p>
+      <p>&copy;{t('copyright')}</p>
+    </footer>
+  )
 }
